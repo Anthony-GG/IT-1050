7d46c0d58860313860e40b6ca809537d12835ff4
Anthony Iacano <iacano5@outlook.com>
2020-05-07 01:00:23 -0400

    -Final Commit for the Class

---
+++ Final Examination/Elevator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Final_Examination
+{
+    class Elevator
+    {
+        private double MaxWeight;
+        private Passenger[] Occupants;
+
+        public Elevator(int maxOccupants, double maxWeight)
+        {
+            this.Occupants = new Passenger[maxOccupants];
+            this.MaxWeight = maxWeight;
+        }
+        public void AddOccupant(Passenger passenger, int index)
+        {
+            Occupants[index] = passenger;
+        }
+
+        public double GetCurrentWeight()
+        {
+            double sum = 0;
+            foreach (Passenger human in Occupants)
+            {
+               sum  += human.GetWeight();
+            }
+
+            return sum;
+        }
+        
+        public bool IsOverMaxCapacity()
+        {
+            return (GetCurrentWeight() > MaxWeight);
+        }
+    }
+}
---
+++ Final Examination/Passenger.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Final_Examination
+{
+    class Passenger
+    {
+        private string Name;
+        private double Weight;
+
+        public Passenger(string name, double weight)
+        {
+            this.Weight = weight;
+            this.Name = name;
+        }
+
+        public string GetName()
+        {
+            return Name;
+        }
+
+        public double GetWeight()
+        {
+            return Weight;
+        }
+    }
+}
