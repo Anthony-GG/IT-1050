00ee923d34bc0218f62221334ffc484546e896c8
Anthony Iacano <iacano5@outlook.com>
2020-05-06 23:13:53 -0400

    Added Assignment 5

---
+++ Assignment 5/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment_5
+{
+    class Person
+    {
+        public string firstName;
+        public string lastName;
+        public string fullName;
+        public Person(string FirstName, string LastName)
+        {
+            this.firstName = FirstName;
+            this.lastName = LastName;
+        }
+
+
+        public void PrintFullName()
+        {
+            fullName = this.firstName + " " + this.lastName;
+            Console.WriteLine(fullName);
+        }
+    }
+}
---
+++ Assignment 5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Assignment_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person Anthony = new Person("Anthony", "Iacano");
+            Person Jeremiah = new Person("Jeremiah", "Ukwela");
+            Person Natalie = new Person("Natalie", "Kozelka");
+
+            Person[] people = new Person[3];
+            people[0] = Anthony;
+            people[1] = Jeremiah;
+            people[2] = Natalie;
+
+            foreach (Person human in people) 
+            {
+                human.PrintFullName();
+            }
+
+        }
+    }
+}
